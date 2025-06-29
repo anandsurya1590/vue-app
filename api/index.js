7dee9a45fce38d0b4b27e743212bd37d6eaf483c
Anand J S <anandsurya1590@gmail.com>
2025-06-29 19:19:30 +0530

    server module exported

--- api/index.js
+++ api/index.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 
-const userRoutes = require('./routes/userRoutes');
+const userRoutes = require("./routes/userRoutes");
 
 app.use(express.json());
-app.use(cors());  
-app.use('/api/users', userRoutes);
+app.use(cors());
+app.use("/api/users", userRoutes);
 
-app.get('/api', async (req, res) => {
-  res.send({ message: 'API is working' });
+app.get("/api", async (req, res) => {
+  res.send({ message: "API is working" });
 });
 
 const PORT = process.env.PORT || 3000;
@@ -19,3 +19,5 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 // Serve static files from the Vue app
+
+module.exports = app;
